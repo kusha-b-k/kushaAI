c7f09f724d9907e303afc96542bd533e1dcaadd4
KUSHA B K <kushabaledonikrishnamurthy@1010011110011.local>
2017-11-10 14:10:17 +0530

    proinit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='machineai',
+      version='1.0',
+      description='Python Artificial intelligence',
+      author='KUSHA B K',
+      author_email='kusha.b.k@gmail.com'
+     )
